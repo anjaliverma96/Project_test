18115359a14aeb5a2aeb0a862537849b06b13b39
anjaliverma96 <anjaliverma2020@u.northwestern.edu>
2019-06-12 15:31:02 -0500

    hh

--- app/bototest.py
+++ app/bototest.py
@@ -7,12 +7,10 @@ class Boto:
 
 		s3_resource = boto3.resource('s3')
 		bucket = s3_resource.Bucket('jokerecommender')
-		bucket.download_file(os.path.join("../data", "ratings.csv"),'ratings.csv')
+		bucket.download_file('ratings.csv',os.path.join("../data", "ratings.csv"))
 
 	def download_jokes(self):
 		
 		s3_resource = boto3.resource('s3')
 		bucket = s3_resource.Bucket('jokerecommender')
-		bucket.download_file(os.path.join("../data", "jokes.csv"),'jokes.csv')
-
-		
+		bucket.download_file('jokes.csv',os.path.join("../data", "jokes.csv"))
