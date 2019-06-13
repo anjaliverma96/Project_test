07714adfda31783dcae85e9a2e4413bfd20b7888
anjaliverma96 <anjaliverma2020@u.northwestern.edu>
2019-06-12 20:18:07 -0500

    message

--- src/recommender.py
+++ src/recommender.py
@@ -17,20 +17,43 @@ class Recommender:
 		self.data_final = data_final
 		self.data_jokes = data_joke
 
+	# Define function that returns the joke that will be displayed as default to all users on
+	# their first visit to the page. 
+
 	def get_most_popular(self):
+
+		"""Finds the joke that is given a rating of 10 by maximum users.
+    Args:
+        None
+
+    Returns:
+        (tuple): Tuple consisting of the top rated textual joke as well as the
+        index at which the joke_id for the top rated joke occurs in the unique list of joke ids
+    """
 		popular_rated = self.data_final[self.data_final['Rating'] == 10]
 		popular_jokes = popular_rated.groupby('JokeID').count().reset_index()
 		popular_jokes = popular_jokes[['JokeID','Rating']]
 		popular_jokes.columns = ['JokeID','Number_rated10']
-		top_joke = popular_jokes.sort_values(by=['Number_rated10']).head(1)
+		top_joke = popular_jokes.sort_values(by=['Number_rated10'], ascending=False).head(1)
 		top_joke_val = top_joke['JokeID'].values[0]
-		jokes_list = self.data_final['JokeID'].unique().tolist()
+		jokes_list = sorted(set(self.data_final['JokeID']))
 		joke_num = jokes_list.index(top_joke_val)
 		top_joke_desc = self.data_jokes[self.data_jokes['JokeID'] == top_joke_val].values[0][1]
 
 		return top_joke_desc, joke_num
 
+	# Define function that transforms data to user-item interaction matrix
+
 	def get_interaction(self):
+
+		"""Transforms the dataframe with user ratings to a user-item interaction matrix.
+    Args:
+        None
+
+    Returns:
+        interaction_matrix(:py:class:`pandas.DataFrame`): Pandas dataframe with unique user records and ratings
+        for each joke by the user
+    """
     
 		#create interaction matrix 
 		interaction_matrix =self.data_final.pivot(index = 'User_ID', columns ='JokeID', values = 'Rating').fillna(0)
@@ -38,7 +61,20 @@ class Recommender:
 
 		return interaction_matrix
 
+	# Define function that appends row with ratings for each joke by a new user, to existing interaction matrix
+
 	def append_new_user(self,interaction_df, user_pref):
+
+		"""Appends user_pref as the last row of the interaction_df.
+    Args:
+        interaction_df (:py:class:`pandas.DataFrame`): DataFrame with unique user records and ratings
+        for each joke by the user (i.e. interaction_matrix)
+        user_pref (:obj:`list`): List with ratings for jokes viewed by new user and 0s for unrated jokes
+     
+    Returns:
+        new_df (:py:class:`pandas.DataFrame`): DataFrame with user_pref appended as the last row
+    """
+
 
 		user_pref = pd.DataFrame(user_pref).T
 		user_pref.columns = list(interaction_df.columns)
@@ -48,24 +84,47 @@ class Recommender:
 		new_df = pd.concat(frames)
 		return new_df
 
+
+	# Define function that decomposes dataframe into 3 simpler matrices and 
+	# gives predicted joke ratings for new user
+
 	def get_svd(self,new_df):
+
+		"""Gets predicted joke ratings for all users in new_df.
+    Args:
+        new_df (:py:class:`pandas.DataFrame`): DataFrame with ratings for each joke by a new user, 
+        appended as the last row of existing interaction matrix
+        
+    Returns:
+        preds_df (:py:class:`pandas.DataFrame`): DataFrame with predicted joke ratings for all users in new_df
+    """
 
 		new_matrix = new_df.as_matrix()
 		
 		#get the mean score of users
 		mean_score = np.mean(new_matrix,axis = 1)
 		new_matrix_dm = new_matrix - mean_score.reshape(-1,1)
-		#decompose interaction matrix
+		#decompose interaction matrix into U, Vt, Sigma
 		U, sigma, Vt = svds(new_matrix_dm, k = 5)
 		sigma = np.diag(sigma)
 		all_user_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + mean_score.reshape(-1, 1)
 		preds_df = pd.DataFrame(all_user_predicted_ratings, columns = new_df.columns)
-
+		preds_df = preds_df.round(1)
 
 		return preds_df
 
+	# Define function that returns recommended joke
 
 	def recommend_joke(self,preds_df,already_rated):
+
+		"""Recommends joke to user.
+    Args:
+        new_df (:py:class:`pandas.DataFrame`): DataFrame with ratings for each joke by a new user, 
+        appended as the last row of existing interaction matrix
+        
+    Returns:
+        preds_df (:py:class:`pandas.DataFrame`): DataFrame with predicted joke ratings for all users in new_df
+    """
 		#already rated is a list which shows which all jokes he has rated
 		#all the jokes which aren't rated will have 0s in them
 
@@ -75,7 +134,7 @@ class Recommender:
 			if already_rated[i]==0:
 				unrated_ind.append(i)
 
-		#now we have all the index of jokes he has not rated
+		#now we have all the indices of jokes user has not rated
 
 		#get the last line
 		last_row = preds_df.iloc[[len(preds_df)-1]].values[0]
@@ -89,7 +148,8 @@ class Recommender:
 				best_rating = last_row[i]
 				joke_num = i
 
-		jokes_list = self.data_final['JokeID'].unique().tolist()
+		jokes_list = sorted(set(self.data_final['JokeID']))
+		#jokes_list = self.data_final['JokeID'].unique().tolist()
 		joke_index = jokes_list[joke_num]
 
 		recommended_joke = self.data_jokes[self.data_jokes['JokeID'] == joke_index].values[0][1]
