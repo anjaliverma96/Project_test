1ba0e33509f9c7850ac0fabf40da2adf0bebf981
anjaliverma96 <anjaliverma2020@u.northwestern.edu>
2019-06-13 00:10:39 -0500

    requirements file run update

---
+++ JokeRecommender/bin/rst2man.py
@@ -0,0 +1,26 @@
+#!/Users/anjaliverma/Documents/NU/Spring 2019/AVC_FinalProject/Project_test/JokeRecommender/bin/python
+
+# Author: 
+# Contact: grubert@users.sf.net
+# Copyright: This module has been placed in the public domain.
+
+"""
+man.py
+======
+
+This module provides a simple command line interface that uses the
+man page writer to output from ReStructuredText source.
+"""
+
+import locale
+try:
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+from docutils.writers import manpage
+
+description = ("Generates plain unix manual documents.  " + default_description)
+
+publish_cmdline(writer=manpage.Writer(), description=description)
